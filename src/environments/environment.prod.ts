78c49b1b21e40a7ea4bc471e76da3b9ece5b608e
Nagy Gergő <h771496@stud.u-szeged.hu>
2021-05-27 21:49:04 +0200

    vegso

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDT5FF9I4Ox_RxXwo4iuwgE3643gcptMhM',
+    authDomain: 'device-55122.firebaseapp.com',
+    projectId: 'device-55122',
+    storageBucket: 'device-55122.appspot.com',
+    messagingSenderId: '640534263365',
+    appId: '1:640534263365:web:7d280811a8a3e93067152c',
+    measurementId: 'G-C95YPXK3LG',
+  },
 };
